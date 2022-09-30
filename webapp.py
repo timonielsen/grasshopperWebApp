192bce54c3c170a957ba9563f3a90b0615c51857
timonielsen <timonielsen@me.com>
2022-09-30 08:09:19 +0200

    added picture and parts to the requirements

--- webapp.py
+++ webapp.py
@@ -4,6 +4,12 @@ import ghhops_server as hs
 import requests
 from bs4 import BeautifulSoup
 
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 app = Flask(__name__)
 hops: hs.HopsFlask = hs.Hops(app)
 
@@ -35,6 +41,23 @@ def wind(city):
     soup = BeautifulSoup(res.text, 'html.parser')
     wind = soup.select('#wob_ws')[0].getText().strip()
     return wind
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+
 
 
 if __name__ == '__main__':
