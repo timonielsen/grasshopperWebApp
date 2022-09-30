4c0198654598dc4cc9e00dd809a2ffb4792101c6
timonielsen <timonielsen@me.com>
2022-09-30 09:37:09 +0200

    fixed bug

--- webapp.py
+++ webapp.py
@@ -37,15 +37,15 @@ def getWindSpeed(city):
 
 
 
-# def wind(city):
-#     headers = {
-#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-#     city = city+" weather"
-#     city = city.replace(" ", "+")
-#     res = requests.get(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
-#     soup = BeautifulSoup(res.text, 'html.parser')
-#     wind = soup.select('#wob_ws')[0].getText().strip()
-#     return wind
+def wind(city):
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    city = city+" weather"
+    city = city.replace(" ", "+")
+    res = requests.get(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    wind = soup.select('#wob_ws')[0].getText().strip()
+    return wind
 
 
 # def graph_visualization():
