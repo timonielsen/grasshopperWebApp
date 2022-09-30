8ad18d8a2cf3746951e528774f0770d715763b9d
timonielsen <timonielsen@me.com>
2022-09-30 07:33:54 +0200

    changed

--- webapp.py
+++ webapp.py
@@ -7,6 +7,8 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 hops: hs.HopsFlask = hs.Hops(app)
 
+
+
 @hops.component(
     "/binmult",
     inputs=[hs.HopsNumber("A"), hs.HopsNumber("B")],
@@ -15,16 +17,25 @@ hops: hs.HopsFlask = hs.Hops(app)
 def BinaryMultiply(a: float, b: float):
     return a * b
 
-# @app.route('/')
-# def homepage():
-#     the_time = "Anne ist lieb"
 
-#     return 
+@hops.component(
+    "/windspeed5",
+    inputs=[hs.HopsString("T")],
+    outputs=[hs.HopsString("w")],
+)
+def getWindSpeed(city):
+    return wind(city)
 
-#     <h1>Hello heroku</h1>
-#     <p>It is currently {time}.</p>
+def wind(city):
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    city = city+" weather"
+    city = city.replace(" ", "+")
+    res = requests.get(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8', headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    wind = soup.select('#wob_ws')[0].getText().strip()
+    return wind
 
-#     <img src="http://loremflickr.com/600/600" />
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
